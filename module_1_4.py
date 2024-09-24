8528958d94d092078a1847c5470698f65f46ddeb
nikita17052001 <nikita.loskutov2001@gmail.com>
2024-09-25 00:30:35 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Put yuor text here: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[:1])
+print(my_string[-1:])
+
